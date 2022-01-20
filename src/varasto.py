1cea2ffb1f8b19ee28c61f8d26efe8e99a9a2a9d
oskari83 <oskpelto@gmail.com>
2022-01-20 20:40:26 +0200

    broke tests

--- src/varasto.py
+++ src/varasto.py
@@ -22,7 +22,7 @@ class Varasto:
 
     def lisaa_varastoon(self, maara):
         if maara < 0:
-            return
+            self.saldo = self.tilavuus
         if maara <= self.paljonko_mahtuu():
             self.saldo = self.saldo + maara
         else:
